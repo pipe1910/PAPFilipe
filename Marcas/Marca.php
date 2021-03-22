638f6422c2e3bad3f22c758ffd95d27b80f448ca
Smokee43 <72324455+Smokee43@users.noreply.github.com>
2021-03-22 12:15:22 +0000

    Guilherme- ListaProduto

--- Marcas/Marca.php
+++ Marcas/Marca.php
@@ -1,27 +1,108 @@
 <?php
 include_once("../includes/body.inc.php");
-top_2();
-$con=mysqli_connect(HOST,USER,PWD,DATABASE);
-$id= intval($_GET["id"]);
-$sql="select * from produtos inner join marcas on produtoMarcaId=marcaId inner join imagens on produtoId=imagemProdutoId where marcaId=".$id;
+top();
+
+$con=mysqli_connect("localhost","root","","pap2021sneakerstore");
+$id=intval($_GET["id"]);
+$sql="select * from produtos inner join imagens on produtoId=imagemProdutoId  inner join marcas on produtoMarcaId=marcaId  where produtoMarcaId=".$id;
 $result=mysqli_query($con,$sql);
-$dados=mysqli_fetch_array($result);
 ?>
-<?php
+<!DOCTYPE HTML>
 
-while ($dados = mysqli_fetch_array($result)) {
-?>
-<div class="col-lg-4"<?php echo $dados['produtoId']?>>
+<html>
 
-    <img class="d-block w-100" src="../images/<?php echo $dados['imagemURL']?>">
-    <h3><?php echo $dados['produtoNome']?> <span class="pull-right"></span></h3>
+<head>
+
+    <title>PHPJabbers.com | Free Car Dealer Website Template - teste</title>
+
+    <meta charset="utf-8" />
+
+    <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+
+    <link rel="stylesheet" href="../assets/bootstrap/css/bootstrap.min.css" />
+
+    <link rel="stylesheet" href="../assets/css/main.css" />
+
+    <noscript><link rel="stylesheet" href="../assets/css/noscript.css" /></noscript>
+
+</head>
+
+<body class="is-preload">
+
+<!-- Wrapper -->
+
+<div id="wrapper">
+
+
+
+    <!-- Header -->
+
+    <header id="header">
+
+        <div class="inner">
+
+    </header>
+
+    <!-- Main -->
+
+    <div id="main">
+
+        <div class="inner">
+
+
+            <div class="container-fluid">
+
+                <div class="row">
+                    <?php
+                    while ($dados=mysqli_fetch_array($result)){
+                        ?>
+
+                        <div class="col-lg-4">
+
+
+                            <a href="../Produtos/ListaProduto.php?id=<?php echo $dados["produtoId"] ?>">
+                                <img class="d-block w-100" src="../images/<?php echo $dados["imagemURL"]?>">
+                            </a>
+                            <h3><?php echo $dados["produtoNome"]?><span class="pull-right"></span></h3>
+
+                        </div>
+                        <?php
+                    }
+                    ?>
+
+
+                </div>
+            </div>
+        </div>
+
+        <br>
+
+    </div>
+
 </div>
-    <?php
-    $dados = mysqli_fetch_array($result);
-}
-?>
-<?php
-bot_2();
-?>
 
+</div>
+
+
+
+<!-- Scripts -->
+
+<script src="../assets/js/jquery.min.js"></script>
+
+<script src="../assets/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+<script src="../assets/js/jquery.scrolly.min.js"></script>
+
+<script src="../assets/js/jquery.scrollex.min.js"></script>
+
+<script src="../assets/js/main.js"></script>
+
+
+
+</body>
+
+</html>
+<?php
+bot();
+?>
 
