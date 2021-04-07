16fec026089eaddcfa167ebe5cbc075615a6f373
Pipe <78794920+pipe1910@users.noreply.github.com>
2021-04-07 21:39:30 +0100

    update- destaque

--- Marcas/Marca.php
+++ Marcas/Marca.php
@@ -6,6 +6,7 @@ $con=mysqli_connect("localhost","root","","pap2021sneakerstore");
 $id=intval($_GET["id"]);
 $sql="select * from produtos inner join marcas on produtoMarcaId=marcaId  where produtoMarcaId=".$id;
 $result=mysqli_query($con,$sql);
+$dados=mysqli_fetch_array($result);
 
 ?>
 <!DOCTYPE HTML>
@@ -55,14 +56,16 @@ $result=mysqli_query($con,$sql);
 
                 <div class="row">
                     <?php
-                    while ($dados=mysqli_fetch_array($result)){
+                   $sql2="select * from imagens inner join produtos on imagemProdutoId = produtoId where imagemDestaque = 'sim'";
+                   $result2= mysqli_query($con,$sql2);
+                    while ($dados2=mysqli_fetch_array($result2)){
                         ?>
 
                         <div class="col-lg-4">
 
 
                             <a href="../Produtos/ListaProduto.php?id=<?php echo $dados["produtoId"] ?>">
-                                <img class="d-block w-100" src="../images/<?php //echo $dados["imagemURL"]?>">
+                                <img class="d-block w-100" src="../<?php echo $dados2["imagemURL"]?>">
                             </a>
                             <h3><?php echo $dados["produtoNome"]?><span class="pull-right"></span></h3>
 
