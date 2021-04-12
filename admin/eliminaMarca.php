2f863513515709edfab0be944d1288b5bd3dc772
Pipe <78794920+pipe1910@users.noreply.github.com>
2021-04-12 16:17:51 +0100

    update - administração praticamente acabada

--- admin/eliminaMarca.php
+++ admin/eliminaMarca.php
@@ -3,7 +3,13 @@
 $con = mysqli_connect("localhost", "root", "", "pap2021sneakerstore");
 $id = intval($_GET['id']);
 $sql = "delete from marcas where marcaId = ".$id;
+$sql2 = "delete from produtos where produtoMarcaId = ".$id;
+$sql3 = "delete from imagens where iamgemPordutoId = ".$id;
 
+mysqli_query($con, $sql3);
+mysqli_query($con, $sql2);
 mysqli_query($con, $sql);
+
+
 header("location:adminMarcas.php");
 ?>
--- admin/eliminaProduto.php
+++ admin/eliminaProduto.php
@@ -1,9 +1,11 @@
 <?php
-
-$con = mysqli_connect("localhost", "root", "", "pap2021sneakerstore");
+include_once("../includes/body.inc.php");
 $id = intval($_GET['id']);
- echo $sql = "delete from produtos where produtoId =".$id;
+echo $sql = "delete from produtos where produtoId = ".$id;
+echo $sql2 = "delete from imagens where imagemProdutoId = ".$id;
 
+mysqli_query($con, $sql2);
 mysqli_query($con, $sql);
+
 header("location:adminProdutos.php");
 ?>
