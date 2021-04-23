05f450837cb4a65813b61172e5cf0e7396245403
pipe1910 <pipevital@gmail.com>
2021-04-23 16:42:45 +0100

    update

--- admin/eliminaMarca.php
+++ admin/eliminaMarca.php
@@ -2,9 +2,10 @@
 
 $con = mysqli_connect("localhost", "root", "", "pap2021sneakerstore");
 $id = intval($_GET['id']);
-$sql = "delete from marcas where marcaId = ".$id;
 $sql2 = "delete from produtos where produtoMarcaId = ".$id;
-$sql3 = "delete from imagens where iamgemPordutoId = ".$id;
+$sql3 = "delete from imagens where iamgemProdutoId = ".$id;
+$sql = "delete from marcas where marcaId = ".$id;
+
 
 mysqli_query($con, $sql3);
 mysqli_query($con, $sql2);
