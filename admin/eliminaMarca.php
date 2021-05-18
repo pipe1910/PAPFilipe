446321bc616d3fd3bca5376d9b8b85b2ffdb2a0e
pcinformatica <pceletronicas525@hotmail.com>
2021-05-18 16:41:06 +0100

    quantidade dos produtos no carrinho

--- admin/eliminaMarca.php
+++ admin/eliminaMarca.php
@@ -2,15 +2,24 @@
 
 $con = mysqli_connect("localhost", "root", "", "pap2021sneakerstore");
 $id = intval($_GET['id']);
-$sql2 = "delete from produtos where produtoMarcaId = ".$id;
-$sql3 = "delete from imagens where iamgemProdutoId = ".$id;
-$sql = "delete from marcas where marcaId = ".$id;
 
 
-mysqli_query($con, $sql3);
-mysqli_query($con, $sql2);
+$sql = "delete from produtos inner join imagens on imagemProdutoId = produtoId where  produtoMarcaId = ".$id;
+
 mysqli_query($con, $sql);
 
+if(mysqli_query($con, $sql)){
+
+    $con = mysqli_connect("localhost", "root", "", "pap2021sneakerstore");
+    $id = intval($_GET['id']);
+
+
+    $sql = "delete from marcas inner join produtos on marcaId = produtoMarcaId  left outer join imagens on imagemProdutoId = produtoId where marcaId =".$id;
+
+    mysqli_query($con, $sql);
+
+
+}
 
 header("location:adminMarcas.php");
 ?>
