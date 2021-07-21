ddd9806a430c081278dbdb9525a5f8f63677baa9
JonathanCaracol <joncarajoinas@gmail.com>
2021-07-21 16:29:10 +0100

    update

--- confirmaRegistar.php
+++ confirmaRegistar.php
@@ -8,13 +8,13 @@ $password=md5(addslashes($_POST['password']));
 $telefone=addslashes($_POST['telemovel']);
 $morada=addslashes($_POST['morada']);
 $login=addslashes($_POST['login']);
-$sql="insert into users(userLogin,userPassword) values('".$nome."','".$password."')";
+$sql="insert into users(userLogin,userPassword) values('".$login."','".$password."')";
 mysqli_query($con,$sql);
 $lastId=mysqli_insert_id($con);
 $sql="insert into perfis(perfilId,perfilNome,perfilMorada,perfilEmail,perfilTelefone) ";
 $sql.=" values(".$lastId.",'".$nome."','".$morada."','".$email."','".$telefone."')";
 echo $sql;
 mysqli_query($con,$sql);
-header("location:index.php");
+header("location:login.php");
 
 ?>
